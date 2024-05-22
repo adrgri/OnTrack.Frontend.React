a20152bbea507c351269f23b06de654e2e26d75f
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-05-22 22:07:36 +0200

    Fix text covering CloseButton in ConfirmDeleteModal

--- src/components/ConfirmDeleteModal/ConfirmDeleteModal.tsx
+++ src/components/ConfirmDeleteModal/ConfirmDeleteModal.tsx
@@ -27,70 +27,76 @@ const ConfirmDeleteModal: React.FC<ConfirmDeleteModalProps> = ({
       onClose={onClose}
       onClick={(event) => event.stopPropagation()}
     >
-      <CloseButton onClick={(event) => onClose(event)} right={10} top={10} />
-      <DialogContent
-        sx={{
-          padding: "0",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          width: "100%",
-        }}
-      >
-        <Typography
+      <Box sx={{ position: "relative" }}>
+        <CloseButton
+          onClick={(event) => onClose(event)}
+          sx={{ position: "absolute", top: 10, right: 10 }}
+        />
+        <DialogContent
           sx={{
-            textAlign: "center",
-            mt: { xs: 3, sm: 5, md: 7 }, // Adjust margin top based on screen size
-            mb: { xs: 1, sm: 2, md: 3 },
-            fontSize: { xs: "1rem", sm: "1.25rem", md: "1.5rem" }, // Adjust font size based on screen size
-            px: { xs: 2, sm: 3, md: 4 }, // Adjust padding on horizontal axis based on screen size
-            wordBreak: "break-word", // Ensure long text breaks properly
-          }}
-        >
-          Czy na pewno chcesz usunąć {itemLabel}{" "}
-          {itemName && (
-            <span style={{ fontWeight: "bold" }}> "{itemName}"</span>
-          )}
-          ?
-        </Typography>
-        <Box
-          sx={{
+            padding: "0",
+            paddingTop: "40px", // Add padding to the top to make space for the close button
             display: "flex",
-            flexDirection: { xs: "column", sm: "row" }, // Stack buttons vertically on small screens
-            justifyContent: "center",
+            flexDirection: "column",
             alignItems: "center",
-            mt: 2,
-            mb: 2,
-            width: "100%", // Ensure the box takes full width
-            px: { xs: 2, sm: 3, md: 4 }, // Adjust padding on horizontal axis based on screen size
+            width: "100%",
           }}
         >
-          <SmallButton
-            variant="contained"
-            type="button"
-            onClick={onDeleteConfirm}
+          <Typography
             sx={{
-              m: 1,
-              width: { xs: "100%", sm: "auto" }, // Full width on extra small screens
-              flexGrow: { xs: 1, sm: 0 }, // Make buttons grow to fill space on small screens
+              textAlign: "center",
+              mt: { xs: 3, sm: 5, md: 7 }, // Adjust margin top based on screen size
+              mb: { xs: 1, sm: 2, md: 3 },
+              fontSize: { xs: "1rem", sm: "1.25rem", md: "1.5rem" }, // Adjust font size based on screen size
+              px: { xs: 2, sm: 3, md: 4 }, // Adjust padding on horizontal axis based on screen size
+              wordBreak: "break-word", // Ensure long text breaks properly
             }}
           >
-            Usuń
-          </SmallButton>
-          <SmallButton
-            variant="contained"
-            onClick={onClose}
+            Czy na pewno chcesz usunąć {itemLabel}{" "}
+            {itemName && (
+              <span style={{ fontWeight: "bold" }}> "{itemName}"</span>
+            )}
+            ?
+          </Typography>
+          <Box
             sx={{
-              m: 1,
-              backgroundColor: "#5E5F7D",
-              width: { xs: "100%", sm: "auto" }, // Full width on extra small screens
-              flexGrow: { xs: 1, sm: 0 }, // Make buttons grow to fill space on small screens
+              display: "flex",
+              flexDirection: { xs: "column", sm: "row" }, // Stack buttons vertically on small screens
+              justifyContent: "center",
+              alignItems: "center",
+              mt: 2,
+              mb: 2,
+              width: "100%", // Ensure the box takes full width
+              px: { xs: 2, sm: 3, md: 4 }, // Adjust padding on horizontal axis based on screen size
             }}
           >
-            Anuluj
-          </SmallButton>
-        </Box>
-      </DialogContent>
+            <SmallButton
+              variant="contained"
+              type="button"
+              onClick={onDeleteConfirm}
+              sx={{
+                m: 1,
+                width: { xs: "100%", sm: "auto" }, // Full width on extra small screens
+                flexGrow: { xs: 1, sm: 0 }, // Make buttons grow to fill space on small screens
+              }}
+            >
+              Usuń
+            </SmallButton>
+            <SmallButton
+              variant="contained"
+              onClick={onClose}
+              sx={{
+                m: 1,
+                backgroundColor: "#5E5F7D",
+                width: { xs: "100%", sm: "auto" }, // Full width on extra small screens
+                flexGrow: { xs: 1, sm: 0 }, // Make buttons grow to fill space on small screens
+              }}
+            >
+              Anuluj
+            </SmallButton>
+          </Box>
+        </DialogContent>
+      </Box>
     </Dialog>
   );
 };
