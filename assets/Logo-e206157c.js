3b6fadcce2fe38d6c326fd3ed9486723f89fc764
Valeriia Rohatynska <valeriiarohatynska@gmail.com>
2024-06-09 18:19:33 +0200

    Updates

--- assets/Logo-e206157c.js
+++ assets/Logo-e206157c.js
@@ -1,4 +1,4 @@
-import{j as N,Y as $t,r as c,Q as It,w as gt,Z as Et,$ as xt,a as oe,g as re,s as Y,d as Te,u as se,e as U,_ as h,f as X,h as ie,z as Se,R as Re,c as tt,o as Lt,O as Ot}from"./index-28f2f15b.js";import{_ as Ft,j as yt,a as J,b as Ue,s as nt,d as ot,f as Dt}from"./Button-1234832a.js";function _t(e){return e==null||Object.keys(e).length===0}function At(e){const{styles:t,defaultTheme:n={}}=e,o=typeof t=="function"?s=>t(_t(s)?n:s):t;return N.jsx($t,{styles:o})}var $={};/**
+import{j as N,Y as $t,r as c,Q as It,w as gt,Z as Et,$ as xt,a as oe,g as re,s as Y,d as Te,u as se,e as U,_ as h,f as X,h as ie,z as Se,R as Re,c as tt,o as Lt,O as Ot}from"./index-1785b6ff.js";import{_ as Ft,j as yt,a as J,b as Ue,s as nt,d as ot,f as Dt}from"./Button-123d9d5f.js";function _t(e){return e==null||Object.keys(e).length===0}function At(e){const{styles:t,defaultTheme:n={}}=e,o=typeof t=="function"?s=>t(_t(s)?n:s):t;return N.jsx($t,{styles:o})}var $={};/**
  * @license React
  * react-is.production.min.js
  *
